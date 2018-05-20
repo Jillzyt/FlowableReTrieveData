1f50284ef1a7e5e904ac6c77ff6a69da066d5e80
Unknown <jillzyt25@gmail.com>
2018-05-20 13:30:03 +0800

    Import URL Path

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 
 var http = require('http') // Import HTTP module
+var url = require('url') // Import URL module
 
-http.createServer(onRequest).listen(8888);  // Port to listen to
+http.createServer(onRequest).listen(8888);
 console.log('Server has started');
 
 function onRequest(request, response){
+  var pathName = url.parse(request.url).pathname
+  console.log('pathname' + pathName);
   response.writeHead(200);
-  response.write('This is a basic application.'); // 
+  response.write('This is the first application');
   response.end();
 }
